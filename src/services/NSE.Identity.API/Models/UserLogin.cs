90980c57714be00f283159f24bfdfefef1808551
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-01-31 21:15:37 -0300

    Gernado o token jwt e retornando ele pela controller

--- src/services/NSE.Identity.API/Models/UserLogin.cs
+++ src/services/NSE.Identity.API/Models/UserLogin.cs
@@ -1,8 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace NSE.Identity.API.Dtos
+namespace NSE.Identity.API.Models
 {
-    public class UserRegistrationDto
+    public class UserLogin
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
@@ -11,8 +11,5 @@ namespace NSE.Identity.API.Dtos
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Password { get; set; }
-
-        [Compare("Password", ErrorMessage = "As senhas não conferem.")]
-        public string ConfirmedPassword { get; set; }
     }
 }
