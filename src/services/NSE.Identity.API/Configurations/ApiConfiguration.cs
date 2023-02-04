bb821dd4b542597b3f3e1bca91034d48733bd8c7
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-02-04 14:14:34 -0300

    Refatoração, criação de ambientes separados e inclusão de extensions methods

---
+++ src/services/NSE.Identity.API/Configurations/ApiConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace NSE.Identity.API.Configurations
+{
+    public static class ApiConfiguration
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UserIdentityConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/services/NSE.Identity.API/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace NSE.Identity.API.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddApiConfiguration();
+
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise Identity API",
+                    Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications",
+                    Contact = new OpenApiContact { Name = "Leonardo Queiroz Rocha", Email = "contato@desenvolvedor.io" },
+                    License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/license/MIT") }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
