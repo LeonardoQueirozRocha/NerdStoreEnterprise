8349d874019a297550371737b112e27256ccb1db
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-02-06 19:50:14 -0300

    Configurando a Web App para realizar a autenticação

---
+++ src/web/NSE.WebApp.MVC/Configurations/IdentityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace NSE.WebApp.MVC.Configurations
+{
+    public static class IdentityConfiguration
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(options =>
+                    {
+                        options.LoginPath = "/login";
+                        options.AccessDeniedPath = "/access-denied";
+                    });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
