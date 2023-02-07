ed83d19873741d102aa5bb818069fcea73f7af73
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-02-06 21:11:51 -0300

    Models, Controller e Views de login de cadastro

--- src/web/NSE.WebApp.MVC/Controllers/IdentityController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentityController.cs
@@ -1,8 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models.Identity;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentityController : Controller
     {
+        [HttpGet]
+        [Route("new-account")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("new-account")]
+        public async Task<IActionResult> Register(UserRegistration userRegistration)
+        {
+            if (!ModelState.IsValid) return View(userRegistration);
+
+            // API - Registro
+
+            if (false) return View(userRegistration);
+
+            // Realizar login na PPA
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid) return View(userLogin);
+
+            // API - Login
+
+            if (false) return View(userLogin);
+
+            // Realizar login na PPA
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
