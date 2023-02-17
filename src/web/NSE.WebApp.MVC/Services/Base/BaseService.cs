99d6168e465a0bcce1b6809ca9b26650939fb22b
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-02-17 09:42:42 -0300

    Segregando responsabilidade para o BaseService para tratar o response

--- src/web/NSE.WebApp.MVC/Services/Base/BaseService.cs
+++ src/web/NSE.WebApp.MVC/Services/Base/BaseService.cs
@@ -1,10 +1,24 @@
 ﻿using NSE.WebApp.MVC.Extensions;
 using System.Net;
+using System.Text;
+using System.Text.Json;
 
 namespace NSE.WebApp.MVC.Services.Base
 {
     public abstract class BaseService
     {
+        protected StringContent GetContent(object data)
+        {
+            return new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+        }
+
+        protected async Task<T> DeserializeResponseObjetct<T>(HttpResponseMessage responseMessage)
+        {
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), options);
+        }
+
         protected bool HandleResponseErrors(HttpResponseMessage response)
         {
             switch (response.StatusCode)
