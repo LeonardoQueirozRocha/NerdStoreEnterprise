e69fe7ee0bb4e712b5d80e71e0d214b98e8fb325
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-04-19 19:36:14 -0300

    Desenvolvendo o command para a manipulação do evento de registro do cliente

---
+++ src/services/NSE.Customers.API/Application/Commands/CustomerRegisterCommand.cs
@@ -0,0 +1,21 @@
+﻿using NSE.Core.Messages;
+
+namespace NSE.Customers.API.Application.Commands
+{
+    public class CustomerRegisterCommand : Command
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public CustomerRegisterCommand(Guid id, string name, string email, string cpf)
+        {
+            AggregateId = id;
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
