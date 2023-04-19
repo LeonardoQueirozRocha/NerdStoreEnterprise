8cd33920bcd1c202ca73bd6023192aaf02e43f13
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-04-19 20:27:31 -0300

    Implementação do MediatR para faze a entrega das mensagens da alteração da entidade

---
+++ src/building blocks/NSE.Core/Mediator/MediatorHandler.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Core.Messages;
+
+namespace NSE.Core.Mediator
+{
+    public class MediatorHandler : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public MediatorHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task PublishEventAsync<T>(T act) where T : Event
+        {
+            await _mediator.Publish(act);
+        }
+
+        public async Task<ValidationResult> SendCommandAsync<T>(T command) where T : Command
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
--- src/services/NSE.Customers.API/Application/Commands/CustomerCommandHandler.cs
+++ src/services/NSE.Customers.API/Application/Commands/CustomerCommandHandler.cs
@@ -1,12 +1,28 @@
-﻿namespace NSE.Customers.API.Application.Commands
-{
-    public class CustomerCommandHandler
-    {
-        public void Handle(CustomerRegisterCommand message)
-        {
-            // validar cmd
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Core.Messages;
+using NSE.Customers.API.Models;
 
-            // persistir na base
+namespace NSE.Customers.API.Application.Commands
+{
+    public class CustomerCommandHandler : CommandHandler, IRequestHandler<CustomerRegisterCommand, ValidationResult>
+    {
+        public async Task<ValidationResult> Handle(CustomerRegisterCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.IsValid()) return message.ValidationResult;
+
+            var customer = new Customer(message.Id, message.Name, message.Email, message.Cpf);
+
+            // Validacoes de negocio
+
+            // Persistir no banco
+            if (true) // Ja existe um cliente com o cpf informado
+            {
+                AddError("Este CPF já está em uso.");
+                return ValidationResult;
+            }
+
+            return message.ValidationResult;
         }
     }
 }
