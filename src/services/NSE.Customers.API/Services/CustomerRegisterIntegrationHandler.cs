0a550bd5ed18f81959e66857c456d4544ede82ca
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-06-20 10:05:43 -0300

    Melhorando a resiliencia da aplicação caso a conexão com o RabbitMQ for perdida

--- src/services/NSE.Customers.API/Services/CustomerRegisterIntegrationHandler.cs
+++ src/services/NSE.Customers.API/Services/CustomerRegisterIntegrationHandler.cs
@@ -21,23 +21,30 @@ namespace NSE.Customers.API.Services
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _bus.RespondAsync<RegisteredUserIntegrationEvent, ResponseMessage>(async request => await RegisterCustomerAsync(request));
-
+            SetResponder();
             return Task.CompletedTask;
         }
 
         private async Task<ResponseMessage> RegisterCustomerAsync(RegisteredUserIntegrationEvent message)
         {
-            ValidationResult success;
+            ValidationResult result;
             var customerCommand = new CustomerRegisterCommand(message.Id, message.Name, message.Email, message.Cpf);
 
             using (var scope = _serviceProvider.CreateScope())
             {
                 var mediator = scope.ServiceProvider.GetRequiredService<IMediatorHandler>();
-                success = await mediator.SendCommandAsync(customerCommand);
+                result = await mediator.SendCommandAsync(customerCommand);
             }
 
-            return new ResponseMessage(success);
+            return new ResponseMessage(result);
         }
+
+        private void SetResponder()
+        {
+            _bus.RespondAsync<RegisteredUserIntegrationEvent, ResponseMessage>(async request => await RegisterCustomerAsync(request));
+            _bus.AdvancedBus.Connected += OnConnect;
+        }
+
+        private void OnConnect(object sender, EventArgs e) => SetResponder();
     }
 }
