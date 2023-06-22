2526778bda0c6bdff9639549ea88a91fa27bc13e
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-06-22 12:21:22 -0300

    Implementanco regras ao salvar um registro no carrinho do cliente

--- src/services/NSE.Cart.API/Model/CustomerCart.cs
+++ src/services/NSE.Cart.API/Model/CustomerCart.cs
@@ -2,6 +2,8 @@
 {
     public class CustomerCart
     {
+        internal const int MAX_ITEM_QUANTITY = 5;
+
         public CustomerCart() { }
 
         public CustomerCart(Guid customerId)
@@ -14,5 +16,30 @@
         public Guid CustomerId { get; set; }
         public decimal TotalValue { get; set; }
         public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+        internal void CalculateCartValue() => TotalValue = Items.Sum(p => p.CalculateValue());
+
+        internal bool CartItemExists(CartItem item) => Items.Any(p => p.ProductId == item.ProductId);
+
+        internal CartItem GetByProductId(Guid productId) => Items.FirstOrDefault(p => p.ProductId == productId);
+
+        internal void AddItem(CartItem item)
+        {
+            if (!item.IsValid()) return;
+
+            item.AttachCart(Id);
+
+            if (CartItemExists(item))
+            {
+                var existingItem = GetByProductId(item.ProductId);
+                existingItem.AddUnits(item.Quantity);
+
+                item = existingItem;
+                Items.Remove(existingItem);
+            }
+
+            Items.Add(item);
+            CalculateCartValue();
+        }
     }
 }
