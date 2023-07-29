4ed7fb6e706edee01bc470b232b733b52e7e21fd
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-07-29 20:18:53 -0300

    feat: Implementação do IntegrationEvent na API de pedidos

---
+++ src/building blocks/NSE.Core/Messages/Integrations/StartedOrderIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace NSE.Core.Messages.Integrations
+{
+    public class StartedOrderIntegrationEvent : IntegrationEvent
+    {
+        public Guid CustomerId { get; set; }
+        public Guid OrderId { get; set; }
+        public int PaymentType { get; set; }
+        public decimal Value { get; set; }
+        public string CardNumber { get; set; }
+        public string CardName { get; set; }
+        public string CardExpirationDate { get; set; }
+        public string CardCvv { get; set; }
+    }
+}
