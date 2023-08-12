4bf6bc081eb784e00de94f8bb91974c1f9747c02
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-08-12 17:33:27 -0300

    refactor: Quebrando o código de remoção dos produtos do estoque em métodos para deixar o código mais limpo

--- src/services/NSE.Catalog.API/Services/CatalogIntegrationHandler.cs
+++ src/services/NSE.Catalog.API/Services/CatalogIntegrationHandler.cs
@@ -35,46 +35,58 @@ public class CatalogIntegrationHandler : BackgroundService
 
     private async Task FreeStockAsync(AuthorizedOrderIntegrationEvent message)
     {
-        using (var scope = _serviceProvider.CreateScope())
+        using var scope = _serviceProvider.CreateScope();
+        var productsInStock = new List<Product>();
+        var productRepository = scope.ServiceProvider.GetRequiredService<IProductRepository>();
+        var products = await GetProductsAsync(message, productRepository);
+
+        if (products.Count != message.Items.Count)
         {
-            var productsInStock = new List<Product>();
-            var productRepository = scope.ServiceProvider.GetRequiredService<IProductRepository>();
+            CancelOrderOutOfStock(message);
+            return;
+        }
 
-            var productIds = string.Join(",", message.Items.Select(x => x.Key));
-            var products = await productRepository.GetProductsByIdAsync(productIds);
+        ProcessProducts(message, productsInStock, products);
 
-            if (products.Count != message.Items.Count)
+        if (productsInStock.Count != message.Items.Count)
+        {
+            CancelOrderOutOfStock(message);
+            return;
+        }
+
+        await UpdateStockAsync(message, productRepository, productsInStock);
+    }
+
+    private static async Task<List<Product>> GetProductsAsync(AuthorizedOrderIntegrationEvent message, IProductRepository productRepository)
+    {
+        var productIds = string.Join(",", message.Items.Select(x => x.Key));
+        return await productRepository.GetProductsByIdAsync(productIds);
+    }
+
+    private async Task UpdateStockAsync(AuthorizedOrderIntegrationEvent message, IProductRepository productRepository, List<Product> productsInStock)
+    {
+        productsInStock.ForEach(product => productRepository.Update(product));
+
+        if (!await productRepository.UnitOfWork.Commit())
+        {
+            throw new DomainException($"Problemas ao atualizar o estoque do pedido {message.OrderId}");
+        }
+
+        var loweredOrder = new OrderWrittenOffFromStockIntegrationEvent(message.CustomerId, message.OrderId);
+        await _bus.PublishAsync(loweredOrder);
+    }
+
+    private static void ProcessProducts(AuthorizedOrderIntegrationEvent message, List<Product> productsInStock, List<Product> products)
+    {
+        foreach (var product in products)
+        {
+            var productQuantity = message.Items.FirstOrDefault(p => p.Key == product.Id).Value;
+
+            if (product.IsAvailable(productQuantity))
             {
-                CancelOrderOutOfStock(message);
-                return;
+                product.RemoveOfStock(productQuantity);
+                productsInStock.Add(product);
             }
-
-            foreach (var product in products)
-            {
-                var productQuantity = message.Items.FirstOrDefault(p => p.Key == product.Id).Value;
-
-                if (product.IsAvailable(productQuantity))
-                {
-                    product.RemoveOfStock(productQuantity);
-                    productsInStock.Add(product);
-                }
-            }
-
-            if (productsInStock.Count != message.Items.Count)
-            {
-                CancelOrderOutOfStock(message);
-                return;
-            }
-
-            productsInStock.ForEach(product => productRepository.Update(product));
-
-            if (!await productRepository.UnitOfWork.Commit())
-            {
-                throw new DomainException($"Problemas ao atualizar o estoque do pedido {message.OrderId}");
-            }
-
-            var loweredOrder = new OrderWrittenOffFromStockIntegrationEvent(message.CustomerId, message.OrderId);
-            await _bus.PublishAsync(loweredOrder);
         }
     }
 
