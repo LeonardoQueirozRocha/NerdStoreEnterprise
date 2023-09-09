9d32a3b8a90132a74cd033947d1947fda46d764f
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-09-09 15:58:31 -0300

    fix: Ajustando o arquivo de configuração do nginx

--- src/web/NSE.WebApp.MVC/Configurations/WebAppConfiguration.cs
+++ src/web/NSE.WebApp.MVC/Configurations/WebAppConfiguration.cs
@@ -3,63 +3,62 @@ using Microsoft.AspNetCore.Localization;
 using NSE.WebApp.MVC.Extensions;
 using System.Globalization;
 
-namespace NSE.WebApp.MVC.Configurations
+namespace NSE.WebApp.MVC.Configurations;
+
+public static class WebAppConfiguration
 {
-    public static class WebAppConfiguration
+    public static void AddMvcConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
-        public static void AddMvcConfiguration(this IServiceCollection services, IConfiguration configuration)
+        services.AddControllersWithViews();
+
+        services.Configure<ForwardedHeadersOptions>(options =>
         {
-            services.AddControllersWithViews();
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+        });
 
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-            });
+        services.Configure<AppSettings>(configuration);
+    }
 
-            services.Configure<AppSettings>(configuration);
-        }
+    public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseForwardedHeaders();
 
-        public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        //if (env.IsDevelopment())
+        //{
+        //    app.UseDeveloperExceptionPage();
+        //}
+        //else
+        //{
+        //    app.UseExceptionHandler("/error/500");
+        //    app.UseStatusCodePagesWithRedirects("/error/{0}");
+        //    app.UseHsts();
+        //}
+
+        app.UseExceptionHandler("/error/500");
+        app.UseStatusCodePagesWithRedirects("/error/{0}");
+        app.UseHsts();
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseIdentityConfiguration();
+
+        var supportedCultures = new[] { new CultureInfo("pt-BR") };
+        app.UseRequestLocalization(new RequestLocalizationOptions
         {
-            app.UseForwardedHeaders();
+            DefaultRequestCulture = new RequestCulture("pt-BR"),
+            SupportedCultures = supportedCultures,
+            SupportedUICultures = supportedCultures
+        });
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/error/500");
-            //    app.UseStatusCodePagesWithRedirects("/error/{0}");
-            //    app.UseHsts();
-            //}
+        app.UseMiddleware<ExceptionMiddleware>();
 
-            app.UseExceptionHandler("/error/500");
-            app.UseStatusCodePagesWithRedirects("/error/{0}");
-            app.UseHsts();
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseIdentityConfiguration();
-
-            var supportedCultures = new[] { new CultureInfo("pt-BR") };
-            app.UseRequestLocalization(new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("pt-BR"),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            });
-
-            app.UseMiddleware<ExceptionMiddleware>();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute("default", "{controller=Catalog}/{action=Index}/{id?}");
-            });
-        }
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute("default", "{controller=Catalog}/{action=Index}/{id?}");
+        });
     }
 }
